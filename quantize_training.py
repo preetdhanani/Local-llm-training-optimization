0818ca8f97a8a4f379ef0f887e83683ccd3369b6
Prit Dhanani <49144354+preetdhanani@users.noreply.github.com>
2026-05-14 14:09:55 +0200

    Merged master into main

--- quantize_training.py
+++
@@ -1,9 +0,0 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
-import torch
-
-bnb_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_compute_dtype=torch.bfloat16,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_use_double_quant=True,
-)
